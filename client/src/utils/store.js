0a0cf817fc9bf61018327bb71e0b11b73d904b4c
Wendy Kobayashi <iamalittleforest@gmail.com>
2021-08-12 22:40:44 -0700

    update comment

--- client/src/utils/store.js
+++ client/src/utils/store.js
@@ -1,7 +1,7 @@
 // import createStore 
 import { createStore } from 'redux';
 
-// importing the reducer file
+// import the reducer file
 import reducers from './reducers';
 
 // create a Redux store that accepts the reducers

69c33c6aa3578b7628c9b57889e4e8c6996d2ada
Wendy Kobayashi <iamalittleforest@gmail.com>
2021-08-14 20:20:36 -0700

    remove 'useStoreContext' from GlobalState.js and replace with 'useDispatch' and 'useSelector' from react-redux

--- client/src/components/ProductItem/index.js
+++ client/src/components/ProductItem/index.js
@@ -1,25 +1,23 @@
+// import react dependencies
 import React from "react";
 import { Link } from "react-router-dom";
-import { pluralize } from "../../utils/helpers"
-import { useStoreContext } from "../../utils/GlobalState";
+import { useDispatch, useSelector } from 'react-redux';
+
+// import utils dependencies
 import { ADD_TO_CART, UPDATE_CART_QUANTITY } from "../../utils/actions";
+import { pluralize } from "../../utils/helpers"
 import { idbPromise } from "../../utils/helpers";
 
 function ProductItem(item) {
-  const [state, dispatch] = useStoreContext();
-
-  const {
-    image,
-    name,
-    _id,
-    price,
-    quantity
-  } = item;
-
-  const { cart } = state
+  const dispatch = useDispatch();
+  const state = useSelector(state => state);
+  const { image, name, _id, price, quantity } = item;
+  const { cart } = state;
 
   const addToCart = () => {
-    const itemInCart = cart.find((cartItem) => cartItem._id === _id)
+    const itemInCart = cart.find((cartItem) => cartItem._id === _id);
+
+    // update cart quantity if there itemInCart exists or add to cart if there 
     if (itemInCart) {
       dispatch({
         type: UPDATE_CART_QUANTITY,
